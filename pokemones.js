9f57eef4c825c6240ac3fe4537938d74dd8d27ac
Leonardo Arguello <Leon-Supr@users.noreply.github.com>
2024-10-07 21:52:55 -0600

    Se logró meter los tipos de cada pokemon en spans separados
    Mediante .map(), agregado a un .join() se consiguió meter los tipos de
    cada pokemon en spans separados, de manera que en el CSS se puedan
    editar sus colores de manera individual

--- pokemones.js
+++ pokemones.js
@@ -1,6 +1,11 @@
 const main = document.querySelector("main");
 main.innerHTML = `<p>Nada aún</p>`;
 
+/**
+ * @typedef Pokemon
+ * @property weight {number}
+ */
+
 async function getNumPokemones() {
     const data = await (
         await fetch("https://pokeapi.co/api/v2/pokemon/") //Si le agrego /n/ al final, es otro pokemon
@@ -9,26 +14,36 @@ async function getNumPokemones() {
 }
 
 async function renderPokemon() {
-    data = await getNumPokemones();
+    const data = await getNumPokemones();
     const URL = "https://pokeapi.co/api/v2/pokemon";
     let wholeCards = "";
 
-
     for (let i = 1; i < data; i++) {
         const pokemonURL = `${URL}/${i}/`;
+        /** @type {Pokemon} */
         const thisPokemon = await (await fetch(pokemonURL)).json();
-        console.log(thisPokemon);
+        /** @type {string[]} */
+        const arrayTipos = [];
+
+        //console.log(thisPokemon);
+        for (let i = 0; i < thisPokemon.types.length; i++) {
+            arrayTipos.push(thisPokemon.types[i].type.name);
+            console.log(`${thisPokemon.name} es de tipo/s: ${arrayTipos}`);
+        }
 
         wholeCards += `
         <div class="card">
             <h2>${thisPokemon.name}</h2>
-            <p>${thisPokemon.height}</p>
+            <p>Altura: ${thisPokemon.height} ft</p> 
+            <p>Tipo: ${thisPokemon.types.
+                // el .map, es como un foreach, que modifica el arreglo original, y hace push de cada elemento modificado a una copia del arreglo
+                // al resultado del .map se le aplica el .join, que separá cada arreglo con un ,
+                map(tipo => `<span class="${tipo.type.name}">${tipo.type.name}</span>`).join(", ")}</p>
         </div>
       `
     }
 
     main.innerHTML = wholeCards;
-
 }
 
 renderPokemon();
